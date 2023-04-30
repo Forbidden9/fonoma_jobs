6b8064c69be8ed9baaeb102005e79135d4675af9
Forbidden <lloduardo@gmail.com>
2023-04-30 17:29:51 -0400

    Edit() - Edit main file (add order router).

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
+from routers.order import order
 
 app = FastAPI(title="Fonoma - Backend Developer Test", version="1.0.0")
+
+# Routers
+app.include_router(order, tags=["order"], prefix="/api/order")
 
 
 @app.get("/")
