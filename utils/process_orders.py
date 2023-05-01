1057d585fdb825383b92decf2a7b4ca103cf5ed6
Forbidden <lloduardo@gmail.com>
2023-04-30 23:57:11 -0400

    Edit() - Edit file process_order (add round).

--- utils/process_orders.py
+++ utils/process_orders.py
@@ -4,4 +4,4 @@ from typing import List
 def process_orders(orders: List, criterion: str) -> float:
     if criterion == "all":
         return round(sum(i.get('price') for i in orders), 2)
-    return round(sum(i.get('price') if i.get('status') == criterion else 0 for i in orders))
+    return round(sum(i.get('price') if i.get('status') == criterion else 0 for i in orders), 2)
